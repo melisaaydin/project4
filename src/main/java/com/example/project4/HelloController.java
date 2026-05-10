b1bb4f41e016ca7708ce00c204e177efd77d1124
Melisa AYDIN <melisa.aydin650@gmail.com>
2026-05-10 17:57:12 +0300

    last version

--- src/main/java/com/example/project4/HelloController.java
+++ src/main/java/com/example/project4/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot on Kubernetes! ";
+        return "Hello from Spring Boot on Kubernetes! Project 4";
     }
 
     @GetMapping("/health")
